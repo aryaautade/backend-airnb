4a407b5685e31e9adc6923b7329ce25832754174
aryaautade <autadea135@gmail.com>
2026-02-18 15:40:50 +0530

    Add files

---
+++ models/listing.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: String,
+  description: String,
+  image: {
+    url: String,
+    filename: String,
+  },
+  price: Number,
+  location: String,
+  country: String,
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  },
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review"
+    }
+  ]
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
